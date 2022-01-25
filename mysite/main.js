b9f09548cacccc03a43fd8a457b6d771bb1dd3d0
Ernest_musau <musaubrian45@gmail.com>
2022-01-25 16:27:57 +0300

    boilerplated most of the html

---
+++ mysite/main.js
@@ -0,0 +1,20 @@
+import './style.css'
+
+const insertHtml = `
+  <div class="landing"> 
+    <div class="inner">
+      <h1>Hi there I'm Ernest</h1>
+      <p class="description"> I am a software engineeer </p>
+      <button>Reach me</button>
+    </div>
+  </div>
+
+  <section class="aboutme">
+  
+  </section>
+  <section class=""projects"> 
+  
+  </section>
+
+`
+document.querySelector('#app').innerHTML = insertHtml
