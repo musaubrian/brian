4de27a9553fec9731daaa5a3718396bb6f46c706
Ernest_musau <musaubrian45@gmail.com>
2022-01-30 17:15:44 +0300

    added the fade-in animation for both desktop and mobile displays 'built' the deployment folder - dist

--- mysite/main.js
+++ mysite/main.js
@@ -2,18 +2,25 @@ import './style.css'
 
 const insertHtml = `
   <div class="landing"> 
-    <div class="inner">
-      <h1>Hi there I'm Ernest</h1>
-      <p class="description"> I am a software engineeer </p>
-      <button>Reach me</button>
+    <div class="overlay">
+      <div class="inner">
+        <h1>Hi 👋, there I'm Ernest</h1>
+        <p class="description"> I am a software engineeer </p>
+        <button>Reach me</button>
+      </div>
     </div>
   </div>
 
   <section class="aboutme">
+    <div>
+      <h2>About me section</h2>
+    </div>
   
   </section>
   <section class=""projects"> 
-  
+    <div class="card">
+      <h2>Contain projects cards and their links</h2>    
+    </div>
   </section>
 
 `
