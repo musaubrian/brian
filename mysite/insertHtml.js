15f34282273ed939edc9e7ff37290bb6501bf2f7
musaubrian <musaubrian15@gmail.com>
2022-02-19 20:10:09 +0300

    Add the about me section
    filled out the about me section,
    added the image and the content

---
+++ mysite/insertHtml.js
@@ -0,0 +1,103 @@
+export const insertHtml = `
+  <div class="landing"> 
+    <div class="overlay">
+      <div class="inner">
+        <div class="pika-container"> 
+        <img src="https://github.com/musaubrian/brian/blob/main/mysite/images/pikachugif.gif?raw=true" class="pika"/>
+         </div>
+        <h1>I'm Ernest</h1>
+        <p class="description"> A software engineeer </p>
+        <a href="#aboutme" class="button">Reach me</a>
+      </div>
+    </div>
+  </div>
+
+  <section class=""projects" id="projects"> 
+    <div class="projects-container">
+      <div class="card">
+        <img src="https://raw.githubusercontent.com/musaubrian/brian/ac60b402a583c563f19579158cf08545ec146cbb/mysite/images/task.svg" />
+
+        <div class="card-content">
+          <p> To-do website that stores your tasks on firebase. View <span> <a href="https://github.com/musaubrian/To-do" target="blank">source code</a></span></p>
+        </div>
+
+        <a href="https://writtey.netlify.app" class="button" target="blank">Live Preview</a>
+      </div>         
+      
+      <div class="card">
+        <img src="https://raw.githubusercontent.com/musaubrian/brian/ac60b402a583c563f19579158cf08545ec146cbb/mysite/images/Finance%20analytics%20svg.svg" />
+
+        <div class="card-content">
+          <p> A finance logger built using <strong>Typescript</strong>. View <span> <a href="https://github.com/musaubrian/vite-projects" target="blank">source code</a></span> </p>
+        </div>
+
+        <a href="https://financeer.netlify.app"target="blank" class="button">Live Preview</a>
+      </div> 
+
+      <div class="card">
+        <img src='https://raw.githubusercontent.com/musaubrian/brian/528209003bfde4e16e8c64c84956438c6c051b90/mysite/images/Python_Outline.svg' />
+
+        <div class="card-content">
+          <p> A simple typing speed tester GUI built using python.</p>
+        </div>
+
+        <a href="https://github.com/musaubrian/typing-speed-tester" target="blank" class="button">View Code</a>
+      </div>                                   
+                 
+    </div>
+  </section>
+
+  <section class="aboutme" id="aboutme">
+    <div class="border">  
+      <div class="about-me-image">
+      </div>    
+      <div class="about-me-content">
+        <div>
+          <p>
+            It is a software development methodology which promotes adaptive planning and continuous improvement of the software being produced. Its main aim is to reduce overhead costs by minimizing documentation.
+          </p>
+          <p>
+            The principles governing it are: customer involvement, embracing change, maintaining simplicity, incremental delivery and people not processes. It is mainly applicable in small to medium scale projects and time critical applications.
+            <span>You can check out my projects <a href="#projects">here</a></span>
+          </p>
+        </div>
+        <div class="about-me-buttons">
+          <a href="#" download="Ernest's_cv" class="button">
+           Email
+          </a>
+          
+          
+        </div>
+
+      </div>
+    </div>  
+  </section>
+  
+  <footer>
+    <div>
+      <ul class="social">
+        <li>
+          <a href="https://twitter.com/nab__ri" target="blank">
+          <i class="fab fa-twitter"></i>
+          </a>
+        </li> 
+
+        <li>
+          <a href="https://github.com/musaubrian" target="blank">
+          <i class="fab fa-github"></i>
+          </a>
+        </li> 
+
+        <li>
+          <a href="https://www.linkedin.com/in/ernest-musau/" target="blank">
+          <i class="fab fa-linkedin-in"></i>
+          </a>
+        </li>      
+      </ul>
+    </div>
+    <div class="bottom">
+      <span class="credits">Made with </span> <Span class="heart"><i class="far fa-heart"></i></Span> <span class="credits"> by 𝕭𝖗𝖎.𝖆𝖓</span>
+    </div>  
+  </footer>
+
+`;
