4de3e549c057e2032341cf87e449bca5130a3ac4
musaubrian <musaubrian15@gmail.com>
2022-06-20 17:40:31 +0300

    bundle site

--- mysite_2/dist/assets/ProjectsView.ad4fa0f4.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as a,a as t,F as s,b as o,N as r}from"./index.085206ad.js";const c=o("h1",null,"Projects page 1",-1),m={__name:"ProjectsView",setup(n){return(_,l)=>(e(),a(s,null,[t(r),c],64))}};export{m as default};
