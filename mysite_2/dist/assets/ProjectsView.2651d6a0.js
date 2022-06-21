f216998b662b6ca91d8c7fc95e03463f50c6d0cf
musaubrian <musaubrian15@gmail.com>
2022-06-21 13:11:27 +0300

    build site for production

---
+++ mysite_2/dist/assets/ProjectsView.2651d6a0.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,a as t,F as s,b as o,N as r}from"./index.38cec9e0.js";const c=o("h1",null,"Projects page 1",-1),m={__name:"ProjectsView",setup(n){return(_,l)=>(e(),a(s,null,[t(r),c],64))}};export{m as default};
