8d45f1f4091e0e69906363e1aabb6cfb5c4d2719
musaubrian <musaubrian15@gmail.com>
2022-12-18 08:18:11 +0300

    chore: update twitter link

--- mysite/src/router/index.js
+++ mysite/src/router/index.js
@@ -1,46 +1,51 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import ProjectsView from '../views/ProjectsView.vue'
-import ContactView from '../views/ContactView.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import ProjectsView from "../views/ProjectsView.vue";
+import ContactView from "../views/ContactView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: AboutView
+      path: "/about",
+      name: "about",
+      component: AboutView,
     },
     {
-      path: '/projects',
-      name: 'projects',
-      component: ProjectsView
+      path: "/projects",
+      name: "projects",
+      component: ProjectsView,
     },
     {
-      path: '/contact',
-      name: 'contact',
-      component: ContactView
+      path: "/contact",
+      name: "contact",
+      component: ContactView,
     },
     {
-     path: '/github',
-     beforeEnter() {location.href = 'http://github.com/musaubrian'}
+      path: "/github",
+      beforeEnter() {
+        location.href = "http://github.com/musaubrian";
+      },
     },
     {
-      path: '/linkedin',
-      beforeEnter() {location.href = 'https://www.linkedin.com/in/ernest-musau/'}
+      path: "/linkedin",
+      beforeEnter() {
+        location.href = "https://www.linkedin.com/in/ernest-musau/";
+      },
     },
     {
-      path: '/twitter',
-      beforeEnter() {location.href = 'https://twitter.com/nab__ri'}
-    }
-  ]
-})
+      path: "/twitter",
+      beforeEnter() {
+        location.href = "https://twitter.com/_musaubrian";
+      },
+    },
+  ],
+});
 
-export default router
+export default router;
