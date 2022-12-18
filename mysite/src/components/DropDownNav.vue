56462849a8dcc021c38390cd185c6269b44541b4
musaubrian <musaubrian15@gmail.com>
2022-12-18 09:51:35 +0300

    chore: setup card using tailwind

--- mysite/src/components/DropDownNav.vue
+++ mysite/src/components/DropDownNav.vue
@@ -1,16 +1,16 @@
 <script setup>
-    import { ref} from 'vue';
-    import {vAutoAnimate} from "@formkit/auto-animate"
+import { ref } from 'vue';
+import { vAutoAnimate } from "@formkit/auto-animate"
 
-    const isOpen = ref(false);
+const isOpen = ref(false);
 </script>
 
 <template>
     <nav v-auto-animate class="dropdown-nav" @click="isOpen = !isOpen">
         <div class="header">
-            <div class="logo">
+            <!-- <div class="logo">
                 <img src="../assets/Artboard.png" alt="">
-            </div>
+            </div> -->
             <div class="dropdown-init">
                 <RouterLink to="#" class="mobile-links">
                     <img src="../assets/menu.svg" alt="menu-icon right">
@@ -23,13 +23,13 @@
                     <a href="/" class="mobile-links">Home</a>
                 </li>
                 <li>
-                    <a href="#projects" class="mobile-links">Projects</a>               
+                    <a href="#projects" class="mobile-links">Projects</a>
                 </li>
                 <li>
-                    <a href="#about-me" class="mobile-links">About me</a>               
+                    <a href="#about-me" class="mobile-links">About me</a>
                 </li>
                 <li>
-                    <a href="#contact" class="mobile-links">Reach me</a>               
+                    <a href="#contact" class="mobile-links">Reach me</a>
                 </li>
             </ul>
         </div>
@@ -38,70 +38,79 @@
 </template>
 
 <style scoped>
-    @import '@/assets/base.css';
-    .dropdown-nav{
-        display: none;
+@import '@/assets/base.css';
+
+.dropdown-nav {
+    display: none;
+}
+
+@media screen and (max-width: 550px) {
+    .dropdown-nav {
+        display: flex;
+        width: 95%;
+        margin: auto;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
     }
-    @media screen and (max-width: 550px){
-        .dropdown-nav{
-            display: flex;
-            width: 95%;
-            margin: auto;
-            flex-direction: column;
-            justify-content: center;
-            align-content: center;         
-        }
-        .header {
-            display: flex;
-            flex-direction: row;
-            flex-wrap: nowrap;
-            justify-content: space-around;
-            align-items: center;
-            border-bottom: 2px outset var(--color-text-black);
-            width: 100%;
-            height: 30%;
-        }
-        .logo {
-            width: 40%;
-            height: 100%;
-            display: flex;
-            justify-content: flex-start;
-            align-items: center;
-        }
-        .logo img {
-            width: 100%;
-            height: 100%;
-        }
-        .dropdown-init{
-            width: 50%;
-            display: flex;
-            justify-content: flex-end;
-            align-content: center;
-        }
-        .dropdown-menu{
-            width: 100%;
-            border-bottom: 2px solid var(--color-text-black);
-        }
-        .dropdown-menu ul{
-            margin: auto;
-            list-style: none;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-content: center;
-        }
-        .dropdown-menu ul li{
-            display: flex;
-            justify-content: center;
-            align-content: center;
-            padding: 0.5rem;
-        }
-        .mobile-links{
-            color: var(--color-text-black);
-            text-decoration: none;
-            font-size: 1.2rem;
-            font-weight: 600;
-        }
+
+    .header {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: space-around;
+        align-items: center;
+        border-bottom: 2px outset var(--color-text-black);
+        width: 100%;
+        height: 30%;
     }
-    
+
+    .logo {
+        width: 40%;
+        height: 100%;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    .logo img {
+        width: 100%;
+        height: 100%;
+    }
+
+    .dropdown-init {
+        width: 50%;
+        display: flex;
+        justify-content: flex-end;
+        align-content: center;
+    }
+
+    .dropdown-menu {
+        width: 100%;
+        border-bottom: 2px solid var(--color-text-black);
+    }
+
+    .dropdown-menu ul {
+        margin: auto;
+        list-style: none;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+    }
+
+    .dropdown-menu ul li {
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        padding: 0.5rem;
+    }
+
+    .mobile-links {
+        color: var(--color-text-black);
+        text-decoration: none;
+        font-size: 1.2rem;
+        font-weight: 600;
+    }
+}
 </style>
--- mysite/src/components/contactSection.vue
+++ mysite/src/components/contactSection.vue
@@ -5,11 +5,9 @@
             <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="true">
                 <input type="hidden" name="form-name" value="contact" />
                 <div class="top">
-
                     <label for="name">Name: <input type="text" name="name" required /></label>
 
                     <label for="email">Email: <input type="email" name="email" required /></label>
-
                 </div>
                 <div class="textarea">
                     <label for="message">
@@ -27,107 +25,116 @@
 </template>
 
 <style scoped>
+h1 {
+    display: none;
+}
 
+.form-container {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    width: 90%;
+    margin: auto;
+    padding: 1rem;
+}
 
+.form-container form {
+    width: 80%;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-content: space-between;
+}
+
+.top {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-content: center;
+    padding: 1rem;
+}
+
+.top label {
+    width: 48%;
+}
+
+.top input {
+    font-size: 1.2rem;
+    border: 2px solid var(--color-text-black);
+    background-color: transparent;
+    padding: 0.5rem;
+    border-radius: 15px;
+    text-align: center;
+    width: 100%;
+}
+
+.textarea {
+    padding: 1rem;
+}
+
+.textarea textarea {
+    padding: 0.5rem;
+    border: 2px solid var(--color-text-black);
+    border-radius: 15px;
+    width: 100%;
+    margin: auto;
+    background: transparent;
+    font-size: 1.2rem;
+}
+
+button {
+    width: 40%;
+    padding: 1rem;
+    border: 3px solid var(--color-text-black);
+    border-radius: 15px;
+    margin: 1rem auto;
+    text-decoration: none;
+    background-color: var(--color-text-black);
+    color: var(--color-text-white);
+    font-size: 1.2rem;
+    font-weight: 650;
+    transition: 300ms;
+}
+
+button:hover {
+    background-color: transparent;
+    padding: 1rem;
+    color: var(--color-text-black);
+}
+
+@media screen and (max-width: 790px) {
+    .form-box {
+        height: auto;
+    }
 
     h1 {
-        display: none;
-    }
-    .form-container {
-        display: flex;
-        justify-content: center;
-        align-content: center;
-        width: 90%;
-        margin: auto;
-        padding: 1rem;
-    }
-    .form-container form {
-        width: 80%;
-        margin: auto;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-content: space-between;
-    }
-    .top {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        align-content: center;
-        padding: 1rem;
-    }
-    .top label {
-        width: 48%;
-    }
-    .top input{
-        font-size: 1.2rem;
-        border: 2px solid var(
-            --color-text-black
-        );
-        background-color: transparent;
-        padding: 0.5rem;
-        border-radius: 15px;
+        display: block;
         text-align: center;
-        width: 100%;
-    }
-    .textarea {
+        margin-top: 1rem;
         padding: 1rem;
-    }
-    .textarea textarea {
-        padding: 0.5rem;
-        border: 2px solid var(--color-text-black);
-        border-radius: 15px;
-        width: 100%;
-        margin: auto;
-        background: transparent;
-        font-size: 1.2rem;
-    }
-    button {
-        width: 40%;
-        padding: 1rem;
-        border: 3px solid var(--color-text-black);
-        border-radius: 15px;
-        margin: 1rem auto;
-        text-decoration: none;
-        background-color: var(--color-text-black);
-        color: var(--color-text-white);
-        font-size: 1.2rem;
-        font-weight: 650;
-        transition: 300ms;
-    }
-    button:hover {
-        background-color: transparent;
-        padding: 1rem;
-        color: var(--color-text-black);
     }
 
-    @media screen and (max-width: 790px){
-        .form-box {
-            height: auto;
-        }
-        h1 {
-            display: block;
-            text-align: center;
-            margin-top: 1rem;
-            padding: 1rem;
-        }
-        .form-container,
-        .form-container form {
-            width: 100%;
-        }
-        .top{
-            flex-direction: column;
-            justify-content: center;
-            align-content: center;
-        }
-        .top label {
-            width: 100%;
-            margin-top: 1rem;
-        }
-        .top input,
-        .textarea textarea {
-            font-size: 1.1rem;
-            padding: 0.7rem;
-        }
+    .form-container,
+    .form-container form {
+        width: 100%;
     }
+
+    .top {
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+    }
+
+    .top label {
+        width: 100%;
+        margin-top: 1rem;
+    }
+
+    .top input,
+    .textarea textarea {
+        font-size: 1.1rem;
+        padding: 0.7rem;
+    }
+}
 </style>
