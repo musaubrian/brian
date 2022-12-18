8df4f72b74b335c83ade759106eb38b5a1edf1e1
musaubrian <musaubrian15@gmail.com>
2022-12-18 11:48:22 +0300

    chore: update footer link

--- mysite/src/components/FooterSection.vue
+++ mysite/src/components/FooterSection.vue
@@ -2,29 +2,31 @@
     <footer>
         <div class="footer-container">
             <span>
-                <RouterLink to="/github">©️2022 musuabrian</RouterLink>
+                <a href="https://bio.link/musaubrian" target="_blank">©️2022 musuabrian</a>
             </span>
         </div>
-        
+
     </footer>
 </template>
 
 <style scoped>
-    footer {
-        width: 100%;
-        height: auto;
-        background-color: #b4aeae;
-    }
-    .footer-container {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    span,
-    a{
-        font-size: small;
-        padding: 0.5rem;
-        color: var(--color-text-black);
-        text-decoration: none;
-    }
+footer {
+    width: 100%;
+    height: auto;
+    background-color: #c9c8c8e7;
+}
+
+.footer-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+span,
+a {
+    font-size: small;
+    padding: 0.5rem;
+    color: var(--color-text-black);
+    text-decoration: none;
+}
 </style>
--- mysite/src/components/ProjectsSection.vue
+++ mysite/src/components/ProjectsSection.vue
@@ -23,8 +23,8 @@ useMotion(targetCard, {
 
 <template>
     <div class="my-4 h-auto" ref="targetCard">
-        <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 w-full px-2 capitalize">
-            <div class="col-span-1 w-full h-auto bg-slate-400 p-4 flex flex-col items-center rounded-lg">
+        <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 w-full px-4 md:px-2 capitalize">
+            <div class="col-span-1 w-full h-auto bg-slate-400 p-4 flex flex-col items-center rounded-lg my-2">
                 <div class="w-full inline-flex items-center justify-center object-cover overflow-hidden mt-1">
                     <img src="../assets/avataaars.png" class="w-3/6" alt="">
                 </div>
@@ -40,11 +40,37 @@ useMotion(targetCard, {
 
                 </div>
             </div>
-            <div class="col-span-1 w-full">
-                <h1>project</h1>
+            <div class="col-span-1 w-full h-auto bg-slate-400 p-4 flex flex-col items-center rounded-lg my-2">
+                <div class="w-full inline-flex items-center justify-center object-cover overflow-hidden mt-1">
+                    <img src="../assets/avataaars.png" class="w-3/6" alt="">
+                </div>
+                <h1 class="text-2xl font-semibold text-center">quickbytes</h1>
+                <p class="text-center text-md my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore,
+                    perspiciatis!
+                </p>
+                <div class="inline-flex items-center justify-evenly w-full">
+                    <a href="https://github.com/musaubrian/quick-bytes" target="_blank"
+                        class="bg-green-600 p-3 text-gray-100 font-semibold rounded-md">source</a>
+                    <a href="https://quickbyts.netlify.app" target="_blank" class="bg-blue-500 p-3 rounded-md">live
+                        site</a>
+
+                </div>
             </div>
-            <div class="col-span-1 w-full">
-                <h1>project</h1>
+            <div class="col-span-1 w-full h-auto bg-slate-400 p-4 flex flex-col items-center rounded-lg my-2">
+                <div class="w-full inline-flex items-center justify-center object-cover overflow-hidden mt-1">
+                    <img src="../assets/avataaars.png" class="w-3/6" alt="">
+                </div>
+                <h1 class="text-2xl font-semibold text-center">quickbytes</h1>
+                <p class="text-center text-md my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore,
+                    perspiciatis!
+                </p>
+                <div class="inline-flex items-center justify-evenly w-full">
+                    <a href="https://github.com/musaubrian/quick-bytes" target="_blank"
+                        class="bg-green-600 p-3 text-gray-100 font-semibold rounded-md">source</a>
+                    <a href="https://quickbyts.netlify.app" target="_blank" class="bg-blue-500 p-3 rounded-md">live
+                        site</a>
+
+                </div>
             </div>
         </div>
     </div>
