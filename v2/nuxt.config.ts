7dd63ba48af42dee9d22536b21b2afb3ea84de73
Ernest <musaubrian15@gmail.com>
2023-03-26 16:26:45 +0300

    chore(analytics): configure vercel analytics

--- v2/nuxt.config.ts
+++ v2/nuxt.config.ts
@@ -1,20 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image-edge"],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge'],
   app: {
     head: {
-      title: "MusauBrian",
+      title: 'MusauBrian',
       meta: [
-        { charset: "utf-8" },
-        { name: "description", content: "portfolio site for @musaubrian" },
-        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+        { charset: 'utf-8' },
+        { name: 'description', content: 'portfolio site for @musaubrian' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
       ],
       link: [
         {
-          rel: "icon",
-          href: "favicon.ico",
+          rel: 'icon',
+          href: 'favicon.ico',
         },
       ],
     },
   },
+  plugins: [{ src: '~/plugins/vercel.js', mode: 'client' }],
 });
