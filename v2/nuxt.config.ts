6336eb67fce49f635175321f34081a1cd2acd9f8
Ernest <musaubrian15@gmail.com>
2023-04-16 13:37:42 +0300

    chore: use link instead of @import

--- v2/nuxt.config.ts
+++ v2/nuxt.config.ts
@@ -1,22 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge', '@nuxtjs/fontaine'],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image-edge", "@nuxtjs/fontaine"],
   app: {
     head: {
-      htmlAttrs: { lang: 'en' },
-      title: 'MusauBrian',
+      htmlAttrs: { lang: "en" },
+      title: "MusauBrian",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'description', content: 'portfolio site for @musaubrian' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        { charset: "utf-8" },
+        { name: "description", content: "portfolio site for @musaubrian" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
       ],
       link: [
         {
-          rel: 'icon',
-          href: 'favicon.ico',
+          rel: "icon",
+          href: "favicon.ico",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+        },
+        {
+          rel: "stylesheet",
+          href: 'https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"',
         },
       ],
     },
   },
-  plugins: [{ src: '~/plugins/vercel.js', mode: 'client' }],
+  plugins: [{ src: "~/plugins/vercel.js", mode: "client" }],
 });
