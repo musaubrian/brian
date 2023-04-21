85b3350a124f135a2ec858e92d8f7b4857c91d4e
musaubrian <musaubrian15@gmail.com>
2023-04-21 11:53:49 +0300

    chore: create a new 404 page

---
+++ v2/error.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="h-[90vh] w-full bg-grey-400 flex flex-col items-center justify-center">
+        <h1 class="text-3xl my-2 font-bold">Seems we are lost</h1>
+        <NuxtLink to="/" class="font-semibold border-2 border-gray-700 text-gray-700 text-xl my-2 rounded-md p-2">Head home?</NuxtLink>
+    </div>
+</template>
