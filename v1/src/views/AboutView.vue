b5c6c1b90655f182c38671cc89bd345a394e0805
musaubrian <musaubrian15@gmail.com>
2023-10-11 17:31:11 +0300

    chore: remove projects from landing page

--- v1/src/views/AboutView.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-	import AboutSectionVue from '../components/AboutSection.vue';
-</script>
-
-<template>
-	<AboutSectionVue/>
-</template>
--- v1/src/views/ContactView.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-import ContactSection from '../components/contactSection.vue';
-</script>
-<template>
-    <ContactSection/>
-</template>
