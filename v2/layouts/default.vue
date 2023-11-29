a2ad1fddad881f6553756bf6847179eaf8c5df96
Ernest <musaubrian15@gmail.com>
2023-11-29 16:42:49 +0300

    Update default.vue

--- v2/layouts/default.vue
+++ v2/layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+  <Script children="src='https://beamanalytics.b-cdn.net/beam.min.js' data-token="160080b3-90d4-4398-a7b7-66bf55a50d2b" async />
   <main class="p-2">
     <slot />
   </main>
